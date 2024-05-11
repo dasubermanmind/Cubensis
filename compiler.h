910c96a60500d9e7805a22de9b5d9868f21deedd
Yev <eliot.ost@protonmail.com>
2024-05-11 18:29:20 -0400

    Defining the lexer functionality

--- compiler.h
+++ compiler.h
@@ -2,6 +2,50 @@
 #define CUBENSIS_H
 // keeps all function/class definitions
 #include <stdio.h>
+#include <stdbool.h>
+
+struct pos
+{
+    int line;
+    int col;
+
+    const char *file_name;
+};
+
+// enums get set to token->type
+enum 
+{
+    TOKE_TYPE_IDENTIFIER,
+    TOKE_TYPE_KEYWORD,
+    TOKE_TYPE_OPERATOR,
+    TOKE_TYPE_SYMBOL,
+    TOKE_TYPE_NUMBER,
+    TOKE_TYPE_STRING,
+    TOKE_TYPE_COMMENT,
+    TOKE_TYPE_NEWLINE
+};
+
+struct token
+{
+    int type;
+    int flags;
+    // share mem
+    union 
+    {
+        char cval;
+        const char *sval;
+        unsigned int inum;
+        unsigned long lnum;
+        unsigned long long llnum;
+        void *any;
+    };
+
+    bool whitespace;
+
+    const char *between_brackets;
+
+};
+
 
 enum
 {
